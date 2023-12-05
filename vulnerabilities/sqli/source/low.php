34e34f3f0443004d4edcd52f4668222ec2565742
Guilherme <guilherme.marinheiro@digibee.com>
2023-12-05 14:36:05 -0800

    Fix vulnerability

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -1,17 +1,20 @@
 <?php
 
-if( isset( $_REQUEST[ 'Submit' ] ) ) {
+if (isset($_REQUEST['Submit'])) {
 	// Get input
-	$id = $_REQUEST[ 'id' ];
+	$id = $_REQUEST['id'];
 
 	switch ($_DVWA['SQLI_DB']) {
 		case MYSQL:
-			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+			// Check database using prepared statement
+			$query  = "SELECT first_name, last_name FROM users WHERE user_id = ?";
+			$stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], $query);
+			mysqli_stmt_bind_param($stmt, 's', $id);
+			mysqli_stmt_execute($stmt);
+			$result = mysqli_stmt_get_result($stmt);
 
 			// Get results
-			while( $row = mysqli_fetch_assoc( $result ) ) {
+			while ($row = mysqli_fetch_assoc($result)) {
 				// Get values
 				$first = $row["first_name"];
 				$last  = $row["last_name"];
@@ -22,20 +25,15 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 
 			mysqli_close($GLOBALS["___mysqli_ston"]);
 			break;
+
 		case SQLITE:
 			global $sqlite_db_connection;
 
-			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
-			#$sqlite_db_connection->enableExceptions(true);
-
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
+			// Check database using prepared statement
+			$query  = "SELECT first_name, last_name FROM users WHERE user_id = ?";
+			$stmt = $sqlite_db_connection->prepare($query);
+			$stmt->bindValue(1, $id, SQLITE3_TEXT);
+			$results = $stmt->execute();
 
 			if ($results) {
 				while ($row = $results->fetchArray()) {
@@ -47,10 +45,10 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 				}
 			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
+				echo "Error in fetch " . $sqlite_db->lastErrorMsg();
 			}
 			break;
-	} 
+	}
 }
 
 ?>
